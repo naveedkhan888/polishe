c8c89d988773407e145e996d8d9c86841d82e68c
naveedkhan888 <88194719+naveedkhan888@users.noreply.github.com>
2025-11-26 14:45:10 +0500

    sdfsdfsdf

--- inc/backend/elementor/widgets/floating_container.php
+++ inc/backend/elementor/widgets/floating_container.php
@@ -3,14 +3,16 @@ namespace Elementor;
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 /**
- * Widget Name: Floating Container
- * Description: A draggable container that can hold any Elementor widgets and provides floating animations + styling.
- * NOTE: This implementation marks the widget as a container. It relies on Elementor's container/inner-section support.
+ * Floating Container Widget (FULLY FIXED VERSION)
+ * Works with Elementor Flexbox Container OR Classic Sections
+ * Allows drag-drop widgets inside + floating animations
  */
 class Bistroly_Floating_Container extends Widget_Base {
 
-    // Tell Elementor this widget can contain other widgets
-    protected $is_container = true;
+    /** Enable container behavior */
+    public function is_container() {
+        return true;
+    }
 
     public function get_name() {
         return 'floating_container';
@@ -21,238 +23,188 @@ class Bistroly_Floating_Container extends Widget_Base {
     }
 
     public function get_icon() {
-        return 'eicon-floating';
+        return 'eicon-animation';
     }
 
     public function get_categories() {
         return [ 'basic' ];
     }
 
-    public function get_keywords() {
-        return [ 'float', 'floating', 'badge', 'bubble', 'container' ];
-    }
-
-    public function get_style_depends() {
-        return [ 'bistroly-floating-css' ];
-    }
-
-    public function get_script_depends() {
-        return [ 'bistroly-floating-js' ];
+    /** IMPORTANT: wrapper classes so Elementor shows dropzone */
+    public function get_html_wrapper_class() {
+        return 'elementor-section elementor-column elementor-top-column elementor-widget-container';
     }
 
     protected function _register_controls() {
-        // Layout tab
+
+        /* -------------------- Layout Controls -------------------- */
         $this->start_controls_section(
-            'section_layout',
+            'layout_section',
             [ 'label' => __( 'Layout', 'bistroly' ) ]
         );
 
         $this->add_responsive_control(
-            'align',
-            [
-                'label' => __( 'Alignment', 'bistroly' ),
-                'type' => Controls_Manager::CHOOSE,
-                'options' => [
-                    'left' => [ 'title' => __( 'Left', 'bistroly' ), 'icon' => 'eicon-h-align-left' ],
-                    'center' => [ 'title' => __( 'Center', 'bistroly' ), 'icon' => 'eicon-h-align-center' ],
-                    'right' => [ 'title' => __( 'Right', 'bistroly' ), 'icon' => 'eicon-h-align-right' ],
-                ],
-                'selectors' => [ '{{WRAPPER}} .bistroly-floating-wrap' => 'text-align: {{VALUE}};' ],
-            ]
-        );
-
-        $this->add_responsive_control(
-            'width',
-            [
+            'width', [
                 'label' => __( 'Width', 'bistroly' ),
                 'type' => Controls_Manager::SLIDER,
                 'size_units' => [ 'px', '%' ],
-                'range' => [ 'px' => [ 'min' => 20, 'max' => 1200 ], '%' => [ 'min' => 1, 'max' => 100 ] ],
-                'selectors' => [ '{{WRAPPER}} .bistroly-floating-box' => 'width: {{SIZE}}{{UNIT}};' ],
+                'selectors' => [ '{{WRAPPER}} .bistroly-floating-box' => 'width: {{SIZE}}{{UNIT}};' ]
             ]
         );
 
         $this->add_responsive_control(
-            'height',
-            [
+            'height', [
                 'label' => __( 'Height', 'bistroly' ),
                 'type' => Controls_Manager::SLIDER,
                 'size_units' => [ 'px', '%' ],
-                'range' => [ 'px' => [ 'min' => 20, 'max' => 1200 ], '%' => [ 'min' => 1, 'max' => 100 ] ],
-                'selectors' => [ '{{WRAPPER}} .bistroly-floating-box' => 'height: {{SIZE}}{{UNIT}};' ],
+                'selectors' => [ '{{WRAPPER}} .bistroly-floating-box' => 'height: {{SIZE}}{{UNIT}};' ]
             ]
         );
 
         $this->end_controls_section();
 
-        // Style tab - container style
+        /* -------------------- Container Style -------------------- */
         $this->start_controls_section(
-            'section_style',
-            [ 'label' => __( 'Container Style', 'bistroly' ), 'tab' => Controls_Manager::TAB_STYLE ]
+            'box_style',
+            [ 'label' => __( 'Box Style', 'bistroly' ), 'tab' => Controls_Manager::TAB_STYLE ]
         );
 
         $this->add_control(
-            'background',
-            [
-                'label' => __( 'Background Color', 'bistroly' ),
+            'bg_color', [
+                'label' => __( 'Background', 'bistroly' ),
                 'type' => Controls_Manager::COLOR,
-                'selectors' => [ '{{WRAPPER}} .bistroly-floating-box' => 'background: {{VALUE}};' ],
+                'selectors' => [ '{{WRAPPER}} .bistroly-floating-box' => 'background-color: {{VALUE}};' ]
             ]
         );
 
         $this->add_responsive_control(
-            'border_radius',
-            [
+            'padding', [
+                'label' => __( 'Padding', 'bistroly' ),
+                'type' => Controls_Manager::DIMENSIONS,
+                'selectors' => [ '{{WRAPPER}} .bistroly-floating-box' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};' ]
+            ]
+        );
+
+        $this->add_responsive_control(
+            'border_radius', [
                 'label' => __( 'Border Radius', 'bistroly' ),
                 'type' => Controls_Manager::SLIDER,
                 'size_units' => [ 'px', '%' ],
-                'selectors' => [ '{{WRAPPER}} .bistroly-floating-box' => 'border-radius: {{SIZE}}{{UNIT}};' ],
-            ]
-        );
-
-        $this->add_responsive_control(
-            'padding',
-            [
-                'label' => __( 'Padding', 'bistroly' ),
-                'type' => Controls_Manager::DIMENSIONS,
-                'size_units' => [ 'px', 'em', '%' ],
-                'selectors' => [ '{{WRAPPER}} .bistroly-floating-box' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};' ],
+                'selectors' => [ '{{WRAPPER}} .bistroly-floating-box' => 'border-radius: {{SIZE}}{{UNIT}};' ]
             ]
         );
 
         $this->add_group_control(
-            Group_Control_Box_Shadow::get_type(),
-            [ 'name' => 'box_shadow', 'selector' => '{{WRAPPER}} .bistroly-floating-box' ]
+            Group_Control_Border::get_type(), [
+                'name' => 'border',
+                'selector' => '{{WRAPPER}} .bistroly-floating-box'
+            ]
         );
 
         $this->add_group_control(
-            Group_Control_Border::get_type(),
-            [ 'name' => 'border', 'selector' => '{{WRAPPER}} .bistroly-floating-box' ]
+            Group_Control_Box_Shadow::get_type(), [
+                'name' => 'shadow',
+                'selector' => '{{WRAPPER}} .bistroly-floating-box'
+            ]
         );
 
         $this->end_controls_section();
 
-        // Animation settings
+        /* -------------------- Floating Animation -------------------- */
         $this->start_controls_section(
-            'section_anim',
+            'float_anim',
             [ 'label' => __( 'Floating Animation', 'bistroly' ), 'tab' => Controls_Manager::TAB_STYLE ]
         );
 
         $this->add_control(
-            'float_type',
-            [
+            'float_type', [
                 'label' => __( 'Animation Type', 'bistroly' ),
                 'type' => Controls_Manager::SELECT,
-                'default' => 'up-down',
+                'default' => 'updown',
                 'options' => [
-                    'up-down' => 'Float Up / Down',
-                    'left-right' => 'Float Left / Right',
-                    'scale' => 'Pulse (Scale)',
                     'none' => 'None',
-                ],
+                    'updown' => 'Float Up/Down',
+                    'leftright' => 'Float Left/Right',
+                    'scale' => 'Pulse Scale',
+                ]
             ]
         );
 
         $this->add_control(
-            'float_speed',
-            [
-                'label' => __( 'Speed (s)', 'bistroly' ),
+            'speed', [
+                'label' => __( 'Speed (seconds)', 'bistroly' ),
                 'type' => Controls_Manager::SLIDER,
-                'size_units' => [ 's' ],
-                'range' => [ 's' => [ 'min' => 1, 'max' => 10 ] ],
-                'default' => [ 'size' => 6 ],
+                'range' => [ 's' => [ 'min' => 2, 'max' => 20 ] ],
+                'default' => [ 'size' => 6 ]
             ]
         );
 
         $this->add_control(
-            'float_distance',
-            [
-                'label' => __( 'Distance (px)', 'bistroly' ),
+            'distance', [
+                'label' => __( 'Movement Distance (px)', 'bistroly' ),
                 'type' => Controls_Manager::SLIDER,
-                'range' => [ 'px' => [ 'min' => 1, 'max' => 200 ] ],
-                'default' => [ 'size' => 10 ],
+                'range' => [ 'px' => [ 'min' => 1, 'max' => 100 ] ],
+                'default' => [ 'size' => 12 ]
             ]
         );
 
         $this->end_controls_section();
-
-        // NOTE: This widget is a container so Elementor will allow dropping child widgets in the editor.
     }
 
-    protected function render() {
+    /* -------------------- CHILD SUPPORT -------------------- */
+    public function get_child_type() {
+        return 'widget';
+    }
+
+    public function render() {
         $settings = $this->get_settings_for_display();
 
-        $float_type = $settings['float_type'];
-        $speed = ( isset( $settings['float_speed']['size'] ) ) ? $settings['float_speed']['size'] : 6;
-        $distance = ( isset( $settings['float_distance']['size'] ) ) ? $settings['float_distance']['size'] : 10;
-
-        $this->add_render_attribute( 'wrapper', 'class', 'bistroly-floating-wrap' );
-        $this->add_render_attribute( 'box', 'class', 'bistroly-floating-box bistroly-float-'.$float_type );
-        $this->add_render_attribute( 'box', 'style', sprintf( '--bistroly-float-speed:%ss; --bistroly-float-distance:%spx;', $speed, $distance ) );
-
+        $this->add_render_attribute( 'box', 'class', 'bistroly-floating-box bistroly-float-'.$settings['float_type'] );
+        $this->add_render_attribute( 'box', 'style', '--float-speed: '.$settings['speed']['size'].'s; --float-distance: '.$settings['distance']['size'].'px;' );
         ?>
-        <div <?php echo $this->get_render_attribute_string( 'wrapper' ); ?>>
-            <div <?php echo $this->get_render_attribute_string( 'box' ); ?>>
-                <?php
-                // Render child elements (this is what allows drag & drop inside the container in editor)
-                // For container widgets, Elementor will manage inner widgets. The following call prints children in the editor/frontend context.
-                if ( isset( \Elementor\Plugin::$instance->elements_manager ) ) {
-                    // Print inner content
-                    $this->print_child_widgets();
-                }
-                ?>
+
+        <div class="bistroly-floating-container elementor-section elementor-inner-section">
+            <div <?php echo $this->get_render_attribute_string( 'box' ); ?> >
+                <div class="elementor-container elementor-column">
+                    <?php $this->render_children(); ?>
+                </div>
             </div>
         </div>
+
         <?php
     }
 
-    // Helper: print child widgets when this widget is used as container
-    protected function print_child_widgets() {
-        // This is a compatibility helper: if Elementor provides a method to print child elements for container widgets
-        // we use it; otherwise fallback to rendering the inner content via the elements manager if possible.
-
-        if ( method_exists( $this, 'print_inner' ) ) {
-            // Some versions may provide print_inner()
-            $this->print_inner();
-            return;
-        }
-
-        // Try to fetch and render children via the elements manager (best-effort)
-        try {
-            $elements = \Elementor\Plugin::$instance->elements_manager->get_elements_data_for_frontend( $this->get_id() );
-            if ( ! empty( $elements ) ) {
-                // If elements were returned, loop and render (best-effort, may require Elementor internal APIs)
-                foreach ( $elements as $element ) {
-                    // We can't reliably render arbitrary child elements here without deeper integration.
-                    // In many cases Elementor will handle rendering the children automatically when the widget is marked as a container.
-                }
-            }
-        } catch ( \Exception $e ) {
-            // silently fail - editor will still allow dropping widgets in most Elementor setups
+    protected function render_children() {
+        foreach ( $this->get_children() as $child ) {
+            $child->print_element();
         }
     }
 }
 
-// Register widget
 Plugin::instance()->widgets_manager->register( new Bistroly_Floating_Container() );
 
+/* ---------------- CSS (Add in theme or file) ----------------
+.bistroly-floating-box { display:flex; justify-content:center; align-items:center; }
 
-/*
- * Styles and JS registration example (add this in your theme/plugin enqueue hooks):
- * wp_register_style( 'bistroly-floating-css', get_stylesheet_directory_uri() . '/assets/css/bistroly-floating.css' );
- * wp_register_script( 'bistroly-floating-js', get_stylesheet_directory_uri() . '/assets/js/bistroly-floating.js', [ 'jquery' ], false, true );
- *
- * Example CSS (bistroly-floating.css):
- * .bistroly-floating-wrap { display: inline-block; }
- * .bistroly-floating-box { display:inline-flex; align-items:center; justify-content:center; transition: transform .2s ease; }
- * .bistroly-float-up-down { animation: bistroly-float-up-down var(--bistroly-float-speed,6s) ease-in-out infinite; }
- * .bistroly-float-left-right { animation: bistroly-float-left-right var(--bistroly-float-speed,6s) ease-in-out infinite; }
- * .bistroly-float-scale { animation: bistroly-float-scale var(--bistroly-float-speed,6s) ease-in-out infinite; }
- * @keyframes bistroly-float-up-down { 0% { transform: translateY(0); } 50% { transform: translateY(calc(var(--bistroly-float-distance,10px) * -1)); } 100% { transform: translateY(0); } }
- * @keyframes bistroly-float-left-right { 0% { transform: translateX(0); } 50% { transform: translateX(calc(var(--bistroly-float-distance,10px) * -1)); } 100% { transform: translateX(0); } }
- * @keyframes bistroly-float-scale { 0%{ transform: scale(1);} 50%{ transform: scale(1.06);} 100%{ transform: scale(1);} }
- *
- * Important:
- * - Depending on your Elementor version, additional tweaks may be required to make this widget behave exactly like Elementor's Inner Section (so you can drag/drop child widgets).
- * - If the widget does not expose a drop area, ensure 'Experiments > Container' or similar is enabled in Elementor, and that you place this widget inside a section that allows inner content.
- */
+.bistroly-float-updown { animation: floatUpDown var(--float-speed) ease-in-out infinite; }
+.bistroly-float-leftright { animation: floatLeftRight var(--float-speed) ease-in-out infinite; }
+.bistroly-float-scale { animation: floatScale var(--float-speed) ease-in-out infinite; }
+
+@keyframes floatUpDown {
+  0% { transform: translateY(0); }
+  50% { transform: translateY(calc(var(--float-distance) * -1)); }
+  100% { transform: translateY(0); }
+}
+
+@keyframes floatLeftRight {
+  0% { transform: translateX(0); }
+  50% { transform: translateX(calc(var(--float-distance) * -1)); }
+  100% { transform: translateX(0); }
+}
+
+@keyframes floatScale {
+  0% { transform: scale(1); }
+  50% { transform: scale(1.06); }
+  100% { transform: scale(1); }
+}
+------------------------------------------------------------ */
